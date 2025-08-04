f1331370ec5462c0591530414fa3d063eadeb665
bruna <bruna@teste.com>
2025-08-04 09:59:22 -0300

    fix(api): ajusta consumo da API para buscar dados dos jogos

--- src/services/api.ts
+++ src/services/api.ts
@@ -42,7 +42,7 @@ type PurchasePayload = {
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://fake-api-tau.vercel.app/api/eplay'
+    baseUrl: 'https://ebac-fake-api.vercel.app/api/eplay'
   }),
   endpoints: (builder) => ({
     getFeaturedGame: builder.query<Game, void>({
